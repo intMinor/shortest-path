8e1c18e9f76c5ef9cabb5006dce110800d923c5b
高涛 <gaotaomo@live.com>
2016-06-10 22:53:50 +0800

    Add project files.

---
+++ shortest path/shortest path.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace shortest_path
+{
+    public class shortest_path
+    {
+        private const double INFINITY = 999999.00;               //定义无限远
+
+        private string[] mixPath;
+
+        private string singlePath = "";
+
+        private double[] mixDistance;
+
+        private bool[] tagPoint;
+
+        private struct graph
+        {
+            public int pathNum;
+            public int pointAllNum;
+            public int pointKnownNum;
+            public string[] pointName;
+            public double[,] pathDisdance;
+        }
+
+        private struct observation
+        {
+            public string starName;
+            public string stopName;
+            public double length;
+        }
+
+        private graph my_graph;
+
+        private observation[] my_observation;
+
+        //构造函数
+        public shortest_path(int pathNum, int pointAllNum, int pointKnownNum, string[] Pname)
+        {
+            mixPath = new string[pointAllNum];
+            Array.Clear(mixPath, 0, pointAllNum);
+
+            mixDistance = new double[pointAllNum];
+            Array.Clear(mixDistance, 0, pointAllNum);
+
+            tagPoint = new bool[pointAllNum];
+            Array.Clear(tagPoint, 0, pointAllNum);
+
+            my_graph.pathNum = pathNum;
+            my_graph.pointAllNum = pointAllNum;
+            my_graph.pointKnownNum = pointKnownNum;
+            my_graph.pointName = new string[pointAllNum];
+            my_graph.pathDisdance = new double[pointAllNum, pointAllNum];
+            Array.Copy(Pname, my_graph.pointName, pointAllNum);
+
+            my_observation = new observation[pathNum];
+            Array.Clear(my_observation, 0, pathNum);
+
+            for (int i = 0; i < pointAllNum; i++)
+                for (int j = 0; j < pointAllNum; j++)
+                    if (i == j)
+                        my_graph.pathDisdance[i, j] = 0.0;
+                    else
+                        my_graph.pathDisdance[i, j] = INFINITY;
+        }
+
+        public void createObservation(int i, string starName, string stopName, double length)
+        {
+            my_observation[i].starName = starName;
+            my_observation[i].stopName = stopName;
+            my_observation[i].length = length;
+        }
+
+        public void createGraph()
+        {
+            int starIndex;
+            int stopIndex;
+
+            for (int i = 0; i < my_graph.pathNum; i++)
+            {
+                starIndex = Array.IndexOf(my_graph.pointName, my_observation[i].starName);
+                stopIndex = Array.IndexOf(my_graph.pointName, my_observation[i].stopName);
+
+                my_graph.pathDisdance[starIndex, stopIndex] = my_observation[i].length;
+                my_graph.pathDisdance[stopIndex, starIndex] = my_observation[i].length;
+            }
+        }
+
+        public void Dijkstra(int currentIndex)
+        {
+            int[] tempPath = new int[my_graph.pointAllNum];
+            Array.Clear(tempPath, 0, my_graph.pointAllNum);
+
+            double tempMix;
+            int Transit;
+
+            for (int i = 0; i < my_graph.pointAllNum; i++)
+            {
+                mixDistance[i] = my_graph.pathDisdance[currentIndex,i];
+
+                tagPoint[i] = false;
+
+                if (my_graph.pathDisdance[currentIndex, i] < INFINITY)
+                    tempPath[i] = currentIndex;
+                else
+                    tempPath[i] = -1;
+            }
+
+            tagPoint[currentIndex] = true;
+            tempPath[currentIndex] = 0;
+
+            for (int i = 0; i < my_graph.pointAllNum - 1; i++)
+            {
+                tempMix = INFINITY;
+                Transit = -1;
+
+                for (int j = 0; j < my_graph.pointAllNum; j++)
+                {
+                    if (tagPoint[j] == false && mixDistance[j] < tempMix)
+                    {
+                        Transit = j;
+                        tempMix = mixDistance[j];
+                    }
+                }
+
+                tagPoint[Transit] = true;
+
+                for (int j = 0; j < my_graph.pointAllNum; j++)
+                {
+                    if (tagPoint[j] == false)
+                    {
+                        if (my_graph.pathDisdance[Transit, j] < INFINITY && mixDistance[Transit] + my_graph.pathDisdance[Transit, j] < mixDistance[j])
+                        {
+                            mixDistance[j] = mixDistance[Transit] + my_graph.pathDisdance[Transit, j];
+                            tempPath[j] = Transit;
+                        }
+                    }
+                }
+            }
+            tidyPath(tempPath, currentIndex);
+        }
+
+        private string[] tidyPath (int[] tempPath, int currentIndex)
+        {
+            for (int i = 0; i < my_graph.pointAllNum; i++)
+            {
+                if (i != currentIndex)
+                {
+                    if (tagPoint[i] == true && mixDistance[i] < INFINITY)
+                    {
+                        recursive(tempPath, i, currentIndex);
+                        mixPath[i] += my_graph.pointName[currentIndex];
+                        mixPath[i] += ",";
+                        mixPath[i] += singlePath;
+                        mixPath[i] += my_graph.pointName[i];
+                        singlePath = "";
+                    }
+                    else
+                    {
+                        mixPath[i] = "";
+                    }
+                }
+            }
+
+            Array.Clear(tempPath, 0, my_graph.pointAllNum);
+
+            return mixPath;
+        }
+
+        private void recursive(int[] tempPath, int i, int currentIndex)
+        {
+            if (tempPath[i] == currentIndex)
+                return;
+            recursive(tempPath, tempPath[i], currentIndex);
+            singlePath += my_graph.pointName[tempPath[i]];
+            singlePath += ",";
+        }
+
+        public string[] getmixPath()
+        {
+            return mixPath;
+        }
+
+        public double[] getmixDistance()
+        {
+            return mixDistance;
+        }
+    }
+}
---
+++ useDijkstra/Program.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using shortest_path;
+using System.IO;
+
+namespace useDijkstra
+{
+    class Program
+    {
+        private struct observation
+        {
+            public string Be;
+            public string En;
+            public double S;
+        }
+
+        private static observation[] my_observation;
+
+        private static string[] Pname;
+
+        static void Main(string[] args)
+        {
+            int Hn = 0;
+            int Un = 0;
+            int Kn = 0;
+
+            string strData = String.Empty;
+            string[] strTemp;
+            int dataIdx = 0;
+
+            FileStream aFile = new FileStream("示例数据.txt", FileMode.Open);
+            StreamReader streamReader = new StreamReader(aFile);
+
+            while (streamReader.Peek() != -1)
+            {
+                dataIdx++;
+
+                //"strData"存储剔除空格逐行读取文档的当前行所有字符，
+                // "strTemp"以数组形式存储当前行，以逗号分隔
+                strData = streamReader.ReadLine().Trim();
+                strTemp = strData.Trim().Split(',');
+
+                if (dataIdx == 1)
+                {
+                    Kn = Convert.ToInt16(strTemp[0]);
+                    Un = Convert.ToInt16(strTemp[1]);
+                    Hn = Convert.ToInt16(strTemp[2]);
+                    Pname = new string[Kn + Un];
+                    my_observation = new observation[Hn];
+                }
+
+                else if (dataIdx == 2)
+                {
+                    for (int i = 0; i < Kn + Un; i++)
+                    {
+                        Pname[i] = Convert.ToString(strTemp[i]);
+                    }
+                }
+
+                else
+                {
+                    my_observation[dataIdx - 3].Be = Convert.ToString(strTemp[0]);
+                    my_observation[dataIdx - 3].En = Convert.ToString(strTemp[1]);
+                    my_observation[dataIdx - 3].S = Convert.ToDouble(strTemp[2]);
+                }
+            }
+
+            Console.WriteLine(" 数据读入成功 ！！！ ");
+
+            Console.WriteLine("/////////////////////////////////////////////////////////////");
+
+            Console.WriteLine(" Hn = {0}, Kn = {1}, Un = {2} ", Hn, Kn, Un);
+
+            Console.WriteLine("/////////////////////////////////////////////////////////////");
+
+            for (int i = 0; i < Kn + Un; i++)
+            {
+                Console.WriteLine("第 {0} 点 ：{1}", i+1, Pname[i]);
+            }
+
+            Console.WriteLine("/////////////////////////////////////////////////////////////");
+
+            for (int i = 0; i < Hn; i++)
+            {
+                Console.WriteLine("起点 ：{0} 终点 ：{1} 距离 ：{2}", my_observation[i].Be, my_observation[i].En, my_observation[i].S.ToString("0.0000"));
+            }
+
+            Console.WriteLine("/////////////////////////////////////////////////////////////");
+
+            string[] singlePath = new string[Kn + Un];
+            string[,] mixPath = new string[Kn + Un, Kn + Un];
+
+            double[] singleDistance = new double[Kn + Un];
+            double[,] mixDistance = new double[Kn + Un, Kn + Un];
+
+            shortest_path.shortest_path my_shortest_path = new shortest_path.shortest_path(Hn, Kn + Un, Kn, Pname);
+
+            for (int i = 0; i < Hn; i++)
+            {
+                my_shortest_path.createObservation(i, my_observation[i].Be, my_observation[i].En, my_observation[i].S);
+            }
+
+            my_shortest_path.createGraph();
+
+            for (int i = 0; i < Un; i++)
+            {
+                my_shortest_path.Dijkstra(i + Kn);
+
+                singlePath = my_shortest_path.getmixPath();
+                singleDistance = my_shortest_path.getmixDistance();
+
+                for (int j = 0; j < Kn + Un; j++)
+                {
+                    mixPath[i, j] = singlePath[j];
+                    mixDistance[i, j] = singleDistance[j];
+                    if (mixDistance[i, j] != 0.0)
+                    {
+                        Console.Write ("mixPath[{0}]:{1}    ", j, mixPath[i, j]);
+                        Console.Write("mixDistance[{0}]:{1}", j, mixDistance[i, j]);
+
+                        Console.Write("\n");
+                    }
+                } 
+                Array.Clear(singlePath, 0, Kn + Un);
+                Array.Clear(singleDistance, 0, Kn + Un);
+            }
+
+            Console.ReadKey();
+            Console.ReadKey();
+            Console.ReadKey();
+        }
+    }
+}
